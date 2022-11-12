f5428e7089b3b4b3868af282fa57ee86507e65ed
Tuan Plus <Dongoctuan2002kk@gmail.com>
2022-11-12 21:13:15 +0700

    add comment

--- customer.h
+++ customer.h
@@ -50,23 +50,24 @@ public:
     friend ostream &operator<<(ostream &os, const Customer &customer); // friend function to overload operator <<
 };
 
+//============== VALIDATION FUNCTIONS ==============
 bool isValidBill(string bill);   // check bill is valid or not
 bool isValidEmail(string email); // check if email is valid
 bool isValidID(string ID);       // check if ID is valid
 bool isValidPhone(string phone); // check if phone is valid
 
+//============== SORT FUNCTIONS ==============
 void quickSort(vector<Customer> &customers, int left, int, int option, bool isAscending); // quick sort
 int partitionAsccending(vector<Customer> &customers, int left, int right, int option);    // partition for quick sort to sort in ascending order
 int partitionDescending(vector<Customer> &customers, int left, int right, int option);    // partition for quick sort to sort in descending order
 
+//============== SEARCH FUNCTIONS ==============
 long searchCustomer(vector<Customer> &customers, string token, int option);     // search customer by ID or free text
 vector<Customer> searchCustomerRegex(vector<Customer> customers, string token); // search customer by regex
 long searchCustomerBinarySearch(vector<Customer> customers, size_t ID);         // search customer by binary search
 long searchCustomerFibonacciSearch(vector<Customer> customers, size_t ID);      // search customer by linear search
-
-Customer setCustomerInformation(vector<Customer> customers, bool isUpdate, long index); // set customer information
-void printAllCustomer(vector<Customer> customers);                                      // print all customer
-
+                                                                                // print all customer
+//============== MAIN FEATURES ==============
 void deleteCustomer(vector<Customer> &customers); // delete customer
 void addCustomer(vector<Customer> &customers);    // add customer
 void updateCustomer(vector<Customer> &customers); // update customer information
@@ -74,12 +75,16 @@ void updateCustomer(vector<Customer> &customers); // update customer information
 vector<Customer> filterBill(vector<Customer> customers, string bill, int option);                          // filter customer by bill (>=, <=, =)
 vector<Customer> filterBill(vector<Customer> customers, unsigned long long start, unsigned long long end); // filter customer by bill between start and end
 
+//============= FORMATTING FUNCTIONS ==============
 string formatAddressForPrint(string address);  // change address to fit in table
 string formatName(string name);                // uppercase first letter of each word
 string formatBill(string bill);                // add comma and currency to bill
 unsigned long long stringToNumber(string str); // convert string to number
 
-void saveData(vector<Customer> customers); // save data to file
-string getLastName(string name);           // get last name of customer
+//============== OTHER FUNCTIONS ==============
+void saveData(vector<Customer> customers);                                              // save data to file
+string getLastName(string name);                                                        // get last name of customer
+Customer setCustomerInformation(vector<Customer> customers, bool isUpdate, long index); // set customer information
+void printAllCustomer(vector<Customer> customers);                                      // print all customer
 
 #endif // CUSTOMER_H
